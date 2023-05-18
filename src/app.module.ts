2728c90e1f4ff3484753a973d2ecd135dfb5bf26
Gary Lin <gary6lin@gmail.com>
2023-05-18 15:09:26 +0800

    GitHub Actions workflow updates.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { FirebaseModule } from 'nestjs-firebase';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './users/user.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.release',
+    }),
     FirebaseModule.forRoot({
       googleApplicationCredential: './firebase-credential.json',
     }),
